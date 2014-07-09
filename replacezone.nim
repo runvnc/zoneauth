c697c1757e1282be27eec8c9d8183e45d0c5de3c
Jason Livesay <ithkuil@gmail.com>
2014-07-09 00:51:49 -0700

    update

---
+++ replacezone.nim
@@ -0,0 +1,19 @@
+#args: domain revipaddr
+import os, osproc, re
+
+var domain = paramStr(1)
+var revip = paramStr(2)
+
+var args="./descrzone " & domain & " " & revip
+var descr=execProcess(args)
+
+var nsdconf = readFile("/etc/nsd.conf")
+
+var tofind = r"\nzone.+\n\tname\:\ " & domain & r"\n(.*)\#x"
+var toreplace = descr
+
+var newconf = nsdconf.replace(re(tofind, {reMultiLine, reDotAll, reExtended, reStudy}), toreplace)
+
+writeFile("/etc/nsd.conf", newconf)
+
+
