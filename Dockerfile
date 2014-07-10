2e390dedb7407b7a2a47c3b330cb7623cef36cf7
Jason Livesay <ithkuil@gmail.com>
2014-07-10 05:59:00 -0700

    update

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@
 FROM dockerfile/nodejs
 ENV DEBIAN_FRONTEND noninteractive
 RUN apt-get -y -q update
-RUN mkdir /etc/nsd3/
-RUN touch /etc/nsd3/nsd.conf
 RUN apt-get -y -q install nsd3 ipv6calc task-spooler
 ADD . /opt/localserver
 RUN cd /opt/localserver
--- replacezone.nim
+++ replacezone.nim
@@ -7,13 +7,13 @@ var revip = paramStr(2)
 var args="./descrzone " & domain & " " & revip
 var descr=execProcess(args)
 
-var nsdconf = readFile("/etc/nsd.conf")
+var nsdconf = readFile("/etc/nsd/nsd.conf")
 
 var tofind = r"\nzone.+\n\tname\:\ " & domain & r"\n(.*)\#x"
 var toreplace = descr
 
 var newconf = nsdconf.replace(re(tofind, {reMultiLine, reDotAll, reExtended, reStudy}), toreplace)
 
-writeFile("/etc/nsd.conf", newconf)
+writeFile("/etc/nsd/nsd.conf", newconf)
 
 
